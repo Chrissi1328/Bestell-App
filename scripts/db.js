f98cabbdfbb7841b73d683892333dbf216458f97
Chrissi1328 <christian.kaindl96web.de>
2025-08-19 16:52:50 +0200

    addes js in db.js

---
+++ scripts/db.js
@@ -0,0 +1,82 @@
+let myDishes = [
+    {
+        "name": "Spaghetti Napoli",
+        "description": "Pasta mit fruchtiger Tomatensauce und frischen Kräutern",
+        "price": 8.90
+    },
+    {
+        "name": "Curry-Hähnchen",
+        "description": "Zartes Hähnchen in cremiger Currysauce mit Reis",
+        "price": 9.80
+    },
+    {
+        "name": "Käsespätzle",
+        "description": "Hausgemachte Spätzle mit Käse und Röstzwiebeln",
+        "price": 8.50
+    },
+    {
+        "name": "Chili sin Carne",
+        "description": "Würziges Bohnen-Chili mit Gemüse und Brot",
+        "price": 8.70
+    },
+    {
+        "name": "Fischstäbchen",
+        "description": "Knusprige Stäbchen mit Kartoffelpüree und Erbsen",
+        "price": 9.20
+    }
+];
+let myDesserts = [
+    {
+        "name": "Tiramisu",
+        "description": "Klassisches italienisches Dessert mit Mascarpone und Espresso",
+        "price": 4.50
+    },
+    {
+        "name": "Schokoladenmousse",
+        "description": "Luftige Mousse au Chocolat mit Zartbitterschokolade",
+        "price": 4.20
+    },
+    {
+        "name": "Apfelstrudel",
+        "description": "Warmer Strudel mit Äpfeln, Rosinen und Zimt, serviert mit Vanillesauce",
+        "price": 4.80
+    },
+    {
+        "name": "Panna Cotta",
+        "description": "Sahniges Dessert mit Fruchtspiegel",
+        "price": 4.60
+    },
+    {
+        "name": "Käsekuchen",
+        "description": "Hausgemachter Käsekuchen mit Mürbeteigboden",
+        "price": 3.90
+    }
+];
+
+let myDrinks = [
+    {
+        "name": "Cola",
+        "description": "Erfrischungsgetränk mit Kohlensäure, 0,5l",
+        "price": 2.50
+    },
+    {
+        "name": "Mineralwasser",
+        "description": "Still oder sprudelnd, 0,5l",
+        "price": 2.00
+    },
+    {
+        "name": "Apfelschorle",
+        "description": "Fruchtige Apfelschorle mit natürlichem Mineralwasser, 0,5l",
+        "price": 2.30
+    },
+    {
+        "name": "Eistee Pfirsich",
+        "description": "Gekühlter schwarzer Tee mit Pfirsichgeschmack, 0,5l",
+        "price": 2.40
+    },
+    {
+        "name": "Orangensaft",
+        "description": "100% Direktsaft aus sonnengereiften Orangen, 0,3l",
+        "price": 2.80
+    }
+];
