0c4754c37da111f0cdf185bd5881acb53a3bae5a
Chrissi1328 <christian.kaindl96web.de>
2025-08-19 16:53:59 +0200

    added js in script.js

---
+++ scripts/script.js
@@ -0,0 +1,144 @@
+let basket_dishes = [];
+
+function init() {
+    let ContentRef = document.getElementById("offer_section")
+    ContentRef.innerHTML = "";
+
+    for (let index = 0; index < myDishes.length; index++) {
+        ContentRef.innerHTML += getDishesTemplate(index);
+    }
+
+    let DessertsRef = document.getElementById("desserts_section");
+    DessertsRef.innerHTML = "";
+
+    for (let index = 0; index < myDesserts.length; index++) {
+        DessertsRef.innerHTML += getDessertsTemplate(index);
+
+    }
+
+    let DrinksRef = document.getElementById("drinks_section");
+    DrinksRef.innerHTML = "";
+
+    for (let index = 0; index < myDrinks.length; index++) {
+        DrinksRef.innerHTML += getDrinksTemplate(index);
+
+    }
+
+
+}
+
+function add_Dish(index) {
+    let dish = myDishes[index];
+
+    for (let i = 0; i < basket_dishes.length; i++) {
+        if (basket_dishes[i].dish.name === dish.name) {
+            basket_dishes[i].quantity += 1;
+            renderBasket();
+            return;
+        }
+    }
+
+    basket_dishes.push({ dish: dish, quantity: 1 });
+    renderBasket()
+    showFeedbackMessage()
+}
+
+function add_Dessert(index) {
+    let dessert = myDesserts[index];
+
+    for (let i = 0; i < basket_dishes.length; i++) {
+        if (basket_dishes[i].dish.name === dessert.name) {
+            basket_dishes[i].quantity += 1;
+            renderBasket();
+            return;
+        }
+    }
+
+    basket_dishes.push({ dish: dessert, quantity: 1 });
+    renderBasket()
+    showFeedbackMessage()
+}
+
+function add_Drink(index) {
+    let drink = myDrinks[index];
+
+    for (let i = 0; i < basket_dishes.length; i++) {
+        if (basket_dishes[i].dish.name === drink.name) {
+            basket_dishes[i].quantity += 1;
+            renderBasket();
+            return;
+        }
+    }
+
+    basket_dishes.push({ dish: drink, quantity: 1 });
+    renderBasket()
+    showFeedbackMessage()
+}
+
+function renderBasket() {
+    let basketRef = document.getElementById("basket");
+    basketRef.innerHTML = `<h2>Warenkorb</h2>`;
+    if (basket_dishes.length === 0) {
+        basketRef.innerHTML += `<p>Ihr Warenkorb ist leer.</p>`;
+        document.getElementById("basket_over").innerHTML = `<button onclick="toggleBasket()" class="remove_button">✖</button><h2>Warenkorb</h2><p>Ihr Warenkorb ist leer.</p>`;
+        return;
+    }
+    let finalSum = 0;
+    for (let i = 0; i < basket_dishes.length; i++) {
+        let item = basket_dishes[i];
+        basketRef.innerHTML += `<p class="dish_basket"><strong>${item.dish.name}</strong></p><p class="p_tag_basket"><button class="basket_buttons" style="cursor:pointer" onclick="changeQuantity(${i}, -1)">-</button>Menge: ${item.quantity}<button class="basket_buttons" style="cursor:pointer" onclick="changeQuantity(${i}, +1)">+</button></p><p class="p_tag_basket">Preis: ${(item.dish.price * item.quantity).toFixed(2)} €</p><div class="delete_div"><button class="delete_button" onclick="removeItem(${i})">🗑</button></div>`;
+        finalSum += item.dish.price * item.quantity;
+    }
+    basketRef.innerHTML += `<div class="basket_finalSum"><p><strong>Gesamtsumme: ${finalSum.toFixed(2)} €</strong></p></div><button class="order_button" onclick="submit_Order()">Bestellen</button`;
+    document.getElementById("basket_over").innerHTML = `<button onclick="toggleBasket()" class="remove_button">✖</button>${basketRef.innerHTML}`;
+}
+
+function removeItem(index) {
+    basket_dishes.splice(index, 1);
+    renderBasket();
+}
+
+function changeQuantity(index, change) {
+    basket_dishes[index].quantity += change;
+
+    if (basket_dishes[index].quantity <= 0) {
+        basket_dishes.splice(index, 1);
+    }
+    renderBasket();
+}
+
+function toggleBasket() {
+    let overlay = document.getElementById("basketOverlay");
+    console.log("overlay:", overlay)
+    renderBasket()
+    overlay.classList.toggle("show");
+
+    let basket_icon = document.getElementById("basket_icon");
+    basket_icon.classList.toggle("show");
+}
+
+function submit_Order() {
+    basket_dishes = [];
+    renderBasket();
+
+    let basketRef = document.getElementById("basket");
+    basketRef.innerHTML = `
+        <h2>Warenkorb</h2>
+        <p class="order_message">Vielen Dank! Deine Bestellung wird nun bearbeitet!</p>
+    `;
+
+    let basketOverlay = document.getElementById("basket_over");
+    if (basketOverlay) {
+        basketOverlay.innerHTML = `
+            <button onclick="toggleBasket()" class="remove_button">✖</button>
+            <h2>Warenkorb</h2>
+            <p class="order_message">Vielen Dank! Deine Bestellung wird nun bearbeitet 🍽️</p>
+        `;
+    }
+}
+
+function showFeedbackMessage() {
+    let msg = document.getElementById("feedbackMessage");
+    msg.classList.add("show");
+    setTimeout(() => msg.classList.remove("show"), 2000);
+}
